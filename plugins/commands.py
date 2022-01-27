a5eee67afcf421d1fdc770ce7e74fb11ab1b1397
Zinan100 <97530806+Zinan100@users.noreply.github.com>
2022-01-27 22:48:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/+uTA_ibAsyWU4OGE1')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -45,10 +45,12 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🔊CHANNEL🔊', url='https://t.me/+uTA_ibAsyWU4OGE1'),
+            InlineKeyboardButton('💬GROUP💬', url='https://t.me/moviespot001100')
+            ],[
+            InlineKeyboardButton('🆘 Help 🆘', callback_data='help'),
+            InlineKeyboardButton('🤠 About 🤠', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
