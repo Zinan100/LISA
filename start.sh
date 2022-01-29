4d7cfeee36781c8453f861cbff4ed9c5bd0d1a69
Zinan100 <97530806+Zinan100@users.noreply.github.com>
2022-01-29 18:57:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /lisa
 fi
-cd /EvaMaria
+cd /lisa
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
